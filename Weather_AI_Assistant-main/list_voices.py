e22edfd1a0a1926f22d1eb7929e536215d9d4ce3
Armin Shafiei <167679515+ArminSHaf@users.noreply.github.com>
2026-02-23 18:32:38 +0200

    Revert "final (#4)"
    This reverts commit de63146c49653d97dd8d4277510b2a9a2e499bb4.

--- Weather_AI_Assistant-main/list_voices.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-    print(f"ID: {voice.id}")
-    print(f"Name: {voice.name}")
-    print(f"Gender: {voice.gender}")
-    print("-" * 10)
